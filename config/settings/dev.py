b2d313d1389c30c59e571d2329653a211135199e
LatifovMuhammadjon <latifov7864@gmail.com>
2025-05-18 23:24:30 +0500

    NB-9: project is created

---
+++ config/settings/dev.py
@@ -0,0 +1,13 @@
+from .base import *
+
+DEBUG = True
+INSTALLED_APPS += [
+]
+
+ALLOWED_HOSTS = ['*']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
